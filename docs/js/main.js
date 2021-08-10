6f4c183cd6df031cf78cf26f8fa56733bc8d101e
Carlos Munoz Diaz <carlos10md@gmail.com>
2021-08-10 14:44:10 +0200

    Primera prueba sin registro correcto de datos

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,8 +1,6 @@
 //Variables globales
-let mymap;
-let svg, g, lines, transform, path;
+let map;
 let currentBtn = 'btnZonasVerdesBA', currentLegend = 'ldZonasVerdesBA';
-let dataOvercrowdBA, dataDistanceBA;
 
 initData();
 
@@ -32,93 +30,45 @@ document.getElementById('btnCallesBA').addEventListener('click', function () {
     }
 });
 
-document.getElementById('layers').addEventListener('change', function(e) {
-    changeLayer(e.target.value);
-});
-
 //Funciones del mapa
 function initData() {
-    //Dejamos pintado el mapa por defecto
-    window.fetch('https://raw.githubusercontent.com/CarlosMunozDiaz/hud-mapas/main/docs/data/buenos_pop_overcrowd.json')
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(response) {
-            dataOvercrowdBA = topojson.feature(response, response.objects['buenos_pop_overcrowd']);
+    mapboxgl.accessToken = 'pk.eyJ1IjoiY2FybG9tdW5veiIsImEiOiJjazcwZW8wZ2sxZWFyM2VtdnQwdjVpMXBuIn0.ltacbidw9otoDaT1RLCNcA';
+    map = new mapboxgl.Map({
+        container: 'mapbox',
+        style: 'mapbox://styles/mapbox/light-v10',
+        center: [-34.6158037, -58.5033387],
+        zoom: 9
+    });
 
-            mymap = L.map('leaflet').setView([-34.6158037, -58.5033387], 9.5);
-
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            }).addTo(mymap);
-
-            L.svg({clickable:true}).addTo(mymap);
-
-            svg = d3.select("#leaflet")
-                .select("svg")
-                .attr("pointer-events", "auto");
-                
-            g = svg.select("g");
-
-            transform = d3.geoTransform({point: projectPoint});
-            path = d3.geoPath().projection(transform);                        
-
-            let lines = g.selectAll("path")
-                .data(dataOvercrowdBA.features)
-                .enter()
-                .append("path")
-                .attr("d", path)
-                .attr("class", 'greenspace')
-                .attr("fill", "#228B22")
-                .attr("fill-opacity", '1');
-
-            mymap.on('viewreset', reset);
-            mymap.on('zoomend', reset);
-
-            function reset(){
-                lines.attr("d", path);
-            }
+    map.on('load', function() {
+        console.log("Entra");
+        //Fuente de datos
+        map.addSource('ba_greenspace', {
+            'type': 'vector',
+            'url': 'mapbox://carlomunoz.8z3ymagb'
         });
+
+        //Layer
+        map.addLayer(
+            {
+            'id': 'layer_ba_greenspace',
+            'source': 'ba_greenspace',
+            'source-layer': 'buenos_pop_overcrowd-7v3q8c',
+            'type': 'fill',
+            'paint': {
+                'fill-color': "red",
+                'fill-opacity': 0.75
+            }
+            }
+        );
+
+        console.log(map.getLayer('layer_ba_greenspace'));
         
-    //Cargamos los datos pesados (tras pintar el mapa y por debajo)
-    window.fetch('https://raw.githubusercontent.com/CarlosMunozDiaz/hud-mapas/main/docs/data/buenos_roads_distance_2.json')
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(response) {
-            dataDistanceBA = topojson.feature(response, response.objects['buenos_roads_distance']);
-
-            let lines = g.selectAll(".roads")
-                .data(dataDistanceBA.features)
-                .enter()
-                .append("path")
-                .attr("d", path)
-                .attr("class", 'roads')
-                .attr("fill", 'none')
-                .attr("stroke-width", '0px')
-                .attr("stroke", 'red');
-
-            mymap.on('viewreset', reset);
-            mymap.on('zoomend', reset);
-
-            function reset(){
-                lines.attr("d", path);
-            }
-        });
+    });
 }
 
 function updateMap(tipo) {
-    if(tipo == 'buenos_pop_overcrowd') {
-
-        g.selectAll('.roads').attr('stroke-width', '0');
-        g.selectAll(".greenspace").attr("fill", '#228B22');
-
-    } else {
-
-        g.selectAll('.greenspace').attr('fill', 'none');
-        g.selectAll('.roads').attr('stroke-width', '.5px');
-
-    }
+    console.log(tipo);
 }
 
 function setBtn(btn) {
@@ -135,38 +85,6 @@ function setLegend(legend) {
         legends[i].classList.remove('active');
     }
     document.getElementById(legend).classList.add('active');
-}
-
-function changeLayer(layer) {
-    if (layer == 'open-street') {
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(mymap);
-    } else if (layer == 'carto') {
-        L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-            //attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-            subdomains: 'abcd',
-            maxZoom: 19
-        }).addTo(mymap);
-    } else if (layer == 'carto-v') {
-        L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png', {
-            //attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-            subdomains: 'abcd',
-            maxZoom: 19
-        }).addTo(mymap);
-    } else if (layer == 'stamen') {
-        L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
-            attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-            subdomains: 'abcd',
-            minZoom: 0,
-            maxZoom: 20,
-            ext: 'png'
-        }).addTo(mymap);
-    } else {
-        L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
-            //attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
-        }).addTo(mymap);
-    }   
 }
 
 /* Helpers */
